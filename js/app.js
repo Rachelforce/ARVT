9d0aabc654e25fa331c5585122b64e7c9ff890ee
Rachelforce <55845125+Rachelforce@users.noreply.github.com>
2022-04-19 20:35:40 +0400

    КЕ

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "%UNITY_WEBGL_BUILD_URL%");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT.json");
 let WebXR;
 window.ARWT = {}
 
